97cd0f56ce063335542f55572b78f18ab546fd4e
Anton Ershov <ant_e@mail.ru>
2024-05-12 13:11:48 +0500

    add fun get_first_equal_last()

--- src/widget.py
+++ src/widget.py
@@ -1,6 +1,7 @@
 import masks
 from datetime import datetime
 
+
 def get_number_from_string(string: str) -> str:
     """
     Функция принимает строку с информацией — тип карты/счета и номер карты/счета.
@@ -12,7 +13,6 @@ def get_number_from_string(string: str) -> str:
     return f'{" ".join(tmp_list[0:-1])} {masks.get_card_number_mask(tmp_list[-1])}'
 
 
-
 def get_date_from_str(str_date: str) -> str:
     """
     Функция, принимает строку вида 2018-07-11T02:26:18.671407
@@ -20,8 +20,25 @@ def get_date_from_str(str_date: str) -> str:
     """
     return str(datetime.strptime(str_date.split('T')[0], "%Y-%m-%d").strftime('%d.%m.%Y'))
 
+
+def get_first_equal_last(str_list: list[str]) -> list[str]:
+    """
+    Функция, принимает на вход список строк и возвращает
+    список строк, в которых первая и последняя буквы совпадают.
+    Если список пустой, верните пустой список.
+    """
+    if len(str_list) == 0:
+        return []
+    new_list = list()
+    for string in str_list:
+        if len(string) > 0 and string[0] == string[-1]:
+            new_list.append(string)
+    return new_list
+
+
 if __name__ == "__main__":
-    print(get_number_from_string('Visa Classic 6831982476737658'))
-    print(get_number_from_string('Maestro 1596837868705199'))
-    print(get_number_from_string('Счет 64686473678894779589'))
-    print(get_date_from_str('2018-07-11T02:26:18.671407'))
+    # print(get_number_from_string('Visa Classic 6831982476737658'))
+    # print(get_number_from_string('Maestro 1596837868705199'))
+    # print(get_number_from_string('Счет 64686473678894779589'))
+    # print(get_date_from_str('2018-07-11T02:26:18.671407'))
+    print(get_first_equal_last(['', 'madam', 'racecar', 'noon', 'level', '']))
