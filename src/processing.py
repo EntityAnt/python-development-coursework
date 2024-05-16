872e6115537dadc7a4fe0f0beaa60521150a3d1c
Anton Ershov <ant_e@mail.ru>
2024-05-16 12:52:34 +0500

    Created processing.py, add fun filter_for_dict()

---
+++ src/processing.py
@@ -0,0 +1,19 @@
+def filter_for_dict(list_dict: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """ Функция, принимает список словарей и значение для ключа state,
+        (опциональный параметр со значением по умолчанию EXECUTED) и возвращает новый список,
+        содержащий только те словари, у которых ключ state содержит переданное в функцию значение. """
+
+    return [i for i in list_dict if i.get('state') == state]
+
+
+def main() -> None:
+    print(filter_for_dict([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+                     ], 'CANCELED')
+          )
+
+
+if __name__ == "__main__":
+    main()
