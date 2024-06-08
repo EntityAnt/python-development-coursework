f669f08732a1fd6f202e874b8ce97405f674aea7
Anton Ershov <ant_e@mail.ru>
2024-06-08 14:40:52 +0500

    Не работает логирование

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import logging
+import os
 
 from src.logger import setup_logging
 from src.masks import get_card_number_mask, get_account_mask, counting_files_and_directories
 
-logger = setup_logging(__name__)
+# logger = setup_logging(__name__)
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# logger.setLevel(logging.INFO)
+# file_handler = logging.FileHandler(os.path.join('logs', '123.log'))
+# file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# file_handler.setFormatter(file_formatter)
+# logger.addHandler(file_handler)
+
 if __name__ == '__main_':
     # Для masks
-    logging.info('123')
-    get_card_number_mask("7000792289606361")
-    get_account_mask("73654108430135874305")
-    counting_files_and_directories(recursive=False)
+    logging.info('123456')
+    # get_card_number_mask("7000792289606361")
+    # get_account_mask("73654108430135874305")
+    # counting_files_and_directories(recursive=False)
